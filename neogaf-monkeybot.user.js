1bbfe26eaa40c22b7b452009148c263ee043574f
Pete Nykänen <pete.a.nykanen@gmail.com>
2017-10-03 10:26:22 +0300

    SemVer version bump

--- neogaf-monkeybot.user.js
+++ neogaf-monkeybot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NeoGAF MonkeyBot
 // @namespace    http://github.com/petetnt/neogaf-monkeybot
-// @version      1.3
+// @version      1.3.0
 // @description  Helper functions for NeoGAF's ModBot posts
 // @author       PeteTNT
 // @match        http://*.neogaf.com/forum/showthread.php?*
